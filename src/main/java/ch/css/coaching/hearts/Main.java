8b52a979adf9ceef5ddf2f51f88737d62e875103
Stephen Wolf <stephenwolf@wolfnet.ch>
2020-11-11 08:46:01 +0100

    Setup helidon
    Includes:
    - health
    - metrics
    - json support

---
+++ src/main/java/ch/css/coaching/hearts/Main.java
@@ -0,0 +1,12 @@
+package ch.css.coaching.hearts;
+
+import java.io.IOException;
+
+public class Main {
+
+    private Main() {}
+
+    public static void main(String[] args) throws IOException {
+        HelidonServer.start();
+    }
+}
