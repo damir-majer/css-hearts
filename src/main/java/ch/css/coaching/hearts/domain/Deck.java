78c47c829685d7a50dfbc0ef2083827d6c25fb38
samue <samuel.degelo@gmail.com>
2020-11-12 14:38:38 +0100

    "mob next [ci-skip]"

--- src/main/java/ch/css/coaching/hearts/domain/Deck.java
+++ src/main/java/ch/css/coaching/hearts/domain/Deck.java
@@ -1,12 +1,32 @@
 package ch.css.coaching.hearts.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface Deck {
+public class Deck {
 
-    void reset();
+    private final List<Card> cards = new ArrayList<>();
 
-    void shuffle();
+    public Deck() {
+        initializeDeck();
+    }
 
-    List<Card> dealNumberOfCards(int number) throws NoCardsAvailableException;
+    private void initializeDeck() {
+        for (int suit = Card.NO_OF_SUITS - 1; suit >= 0; suit--) {
+            for (int rank = Card.NO_OF_RANKS - 1; rank >= 0 ; rank--) {
+                cards.add(new Card(rank, suit));
+            }
+        }
+    }
+
+    public List<Card> dealNumberOfCards(int number) throws NoCardsAvailableException {
+        if (number > cards.size()) {
+            throw new NoCardsAvailableException("Not that many cards available in deck.");
+        }
+        List<Card> result = new ArrayList<>();
+        while (result.size() < number) {
+            result.add(cards.remove(0));
+        }
+        return result;
+    }
 }
--- src/test/java/ch/css/coaching/hearts/domain/DeckTest.java
+++ src/test/java/ch/css/coaching/hearts/domain/DeckTest.java
@@ -5,23 +5,25 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class GameOfHeartsDeckTest {
+class DeckTest {
 
     @Test
     void dealNumberOfCards_Deal13Cards_Received13Cards() throws NoCardsAvailableException {
-        Deck cut = new GameOfHeartsDeck();
+        Deck cut = new Deck();
+
         List<Card> cards = cut.dealNumberOfCards(13);
 
         assertThat(cards).hasSize(13);
     }
 
     @Test
-    void reset() throws NoCardsAvailableException {
-        Deck cut = new GameOfHeartsDeck();
+    void dealNumberOfCards_DealMoreCardsThanAvailable_ThrowsException() throws NoCardsAvailableException {
+        Deck cut = new Deck();
+
         cut.dealNumberOfCards(42);
-        cut.reset();
-        assertThat(cut.dealNumberOfCards(52)).hasSize(52);
+
+        assertThatThrownBy(() -> cut.dealNumberOfCards(52)).isInstanceOf(NoCardsAvailableException.class);
     }
 }
