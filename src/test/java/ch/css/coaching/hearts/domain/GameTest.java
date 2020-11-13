64d9cee0da9d6a294d04d34becd8a57349109484
Wermi <wermi@users.noreply.github.com>
2020-11-13 11:01:27 +0100

    "mob next [ci-skip]"

--- src/test/java/ch/css/coaching/hearts/domain/GameTest.java
+++ src/test/java/ch/css/coaching/hearts/domain/GameTest.java
@@ -1,18 +1,40 @@
 package ch.css.coaching.hearts.domain;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class GameTest {
 
     @Test
     void acceptanceTest() {
-        List<Player> playerList = asList(new Player(), new Player(), new Player(), new Player());
-        Game sut = new Game(new Deck(), playerList);
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Player player4 = new Player();
+        List<Player> playerList = asList(player1, player2, player3, player4);
+        Deck deckMock = mock(Deck.class);
+
+        when(deckMock.dealNumberOfCards(any()))
+                .thenReturn(List.of(new Card(Card.ACE, Card.CLUBS), new Card(Card.ACE, Card.HEARTS)))
+                .thenReturn(List.of(new Card(Card.KING, Card.CLUBS), new Card(Card.KING, Card.HEARTS)))
+                .thenReturn(List.of(new Card(Card.QUEEN, Card.CLUBS), new Card(Card.QUEEN, Card.HEARTS)))
+                .thenReturn(List.of(new Card(Card.JACK, Card.CLUBS), new Card(Card.JACK, Card.HEARTS)));
+
+        Game sut = new Game(deckMock, playerList);
+        GameState initialGameState = sut.start();
+        sut.playMove()
+
+
+        assertThat(lastGameState.getWinner()).contains(player3);
+
 
         //for number players dealCards
         //determine first player
